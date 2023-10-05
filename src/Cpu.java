e2bc375dc0f7af0712168cd554c7f6d850083f03
TheJagermeister <pedroservano007@gmail.com>
2023-10-05 16:22:25 +0200

    Update

--- src/Cpu.java
+++ src/Cpu.java
@@ -1,2 +1,12 @@
 public class Cpu {
+
+    public String brand;
+
+    public String model;
+
+    public Double price;
+
+    public String getFullName(){
+        return brand + " " + model + " " + price;
+    }
 }
